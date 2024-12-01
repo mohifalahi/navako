cf909649429b26309fe281a2879efddc6ecc983c
Mohammad Falahi <45416720+mohifalahi@users.noreply.github.com>
2024-12-01 23:20:37 +0330

    test: health and predict view tests added (#2)

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,70 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HealthCheckViewTest(TestCase):
+    def test_healthcheck_success(self):
+        response = self.client.get(reverse('health-check'))
+        json_response = response.json()
+        self.assertIsNotNone(response)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(json_response['status'], 'ok')
+
+    def test_healthcheck_fail_mehtod_not_allowed(self):
+        response = self.client.post(reverse('health-check'))
+        json_response = response.json()
+        self.assertEqual(response.status_code, 405)
+        self.assertEqual(json_response['detail'], "Method \"POST\" not allowed.")
+
+
+class PredictviewTest(TestCase):
+    def test_predict_success(self):
+        response = self.client.post(reverse('predict'), data={"text": "سلام"})
+        json_response = response.json()
+        self.assertEqual(response.status_code, 200)
+        label = json_response['prediction'][0]['label']
+        score = json_response['prediction'][0]['score']
+        self.assertIn(label, ['recommended', 'no_idea', 'not_recommended'])
+        self.assertLess(score, 1)
+
+    def test_predict_success_case_1(self):
+        response = self.client.post(reverse('predict'), data={"text": "عاشقتم"})
+        json_response = response.json()
+        self.assertEqual(response.status_code, 200)
+        label = json_response['prediction'][0]['label']
+        score = json_response['prediction'][0]['score']
+        self.assertEqual(label, "recommended")
+        self.assertLess(score, 1)
+
+    def test_predict_success_case_2(self):
+        response = self.client.post(reverse('predict'), data={"text": "متنفرم"})
+        json_response = response.json()
+        self.assertEqual(response.status_code, 200)
+        label = json_response['prediction'][0]['label']
+        score = json_response['prediction'][0]['score']
+        self.assertEqual(label, "not_recommended")
+        self.assertLess(score, 1)
+
+    def test_predict_success_case_3(self):
+        response = self.client.post(reverse('predict'), data={"text": "خوشحالم"})
+        json_response = response.json()
+        self.assertEqual(response.status_code, 200)
+        label = json_response['prediction'][0]['label']
+        score = json_response['prediction'][0]['score']
+        self.assertEqual(label, "recommended")
+        self.assertLess(score, 1)
+
+    def test_predict_success_case_4(self):
+        response = self.client.post(reverse('predict'), data={"text": "غمگینم"})
+        json_response = response.json()
+        self.assertEqual(response.status_code, 200)
+        label = json_response['prediction'][0]['label']
+        score = json_response['prediction'][0]['score']
+        self.assertEqual(label, "not_recommended")
+        self.assertLess(score, 1)
+
+    def test_predict_fail_method_not_allowed(self):
+        response = self.client.get(reverse('predict'), data={"text": "غمگینم"})
+        json_response = response.json()
+        self.assertEqual(response.status_code, 405)
+        self.assertEqual(json_response['detail'], "Method \"GET\" not allowed.")
